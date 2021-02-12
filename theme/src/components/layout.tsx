65aed4d2e49cfae937f6615074581cbc718c2791
Eran Kampf <eran@ekampf.com>
2021-02-12 12:55:22 -0800

    Add code-titles, prismjs and smartypants

--- theme/src/components/layout.tsx
+++ theme/src/components/layout.tsx
@@ -1,8 +1,8 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { Global, css } from "@emotion/core";
 import React, { PropsWithChildren } from "react";
 import { Helmet } from "react-helmet";
-import { Flex } from "theme-ui";
+import { jsx, Flex } from "theme-ui";
 import Footer from "./footer";
 import Header from "./header";
 
@@ -20,6 +20,30 @@ export default function Layout({ children }: PropsWithChildren<LayoutProps>) {
       <Helmet>
         <meta charSet="utf-8" />
       </Helmet>
+      <Global
+        styles={css`
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            font-smoothing: antialiased;
+          }
+
+          .gatsby-code-title {
+            margin-bottom: -0.6rem;
+            padding: 0.25rem 1rem;
+            background-color: #dddddd;
+            color: #5c6e74;
+            font-size: 14px;
+            font-weight: 400;
+            border-top-left-radius: 0.3em;
+            border-top-right-radius: 0.3em;
+            z-index: 0;
+          }
+        `}
+      />
       <Header />
       <main>{children}</main>
       <Footer />
