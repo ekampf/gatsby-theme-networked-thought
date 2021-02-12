98ad2e9b22f8752ccfc032cea3d2fe07b91ad38e
Eran Kampf <eran@ekampf.com>
2021-02-12 01:00:13 -0800

    Obsolete template

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -5,9 +5,6 @@ module.exports = () => ({
       resolve: "gatsby-plugin-mdx",
       options: {
         extensions: [".mdx", ".md"],
-        defaultLayouts: {
-          default: require.resolve("./src/templates/page.tsx"),
-        },
         gatsbyRemarkPlugins: [
           {
             resolve: "gatsby-remark-images",
--- theme/src/templates/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { PageProps } from "gatsby";
-
-export default function Home({ children }: PageProps) {
-  return <>{children}</>;
-}
