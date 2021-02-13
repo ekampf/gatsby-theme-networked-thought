003f268e35f0adc0bc13a463d2ca4cdce8b581c9
Eran Kampf <eran@ekampf.com>
2021-02-13 00:26:54 -0800

    header to useStaticQuery

--- theme/src/components/header.tsx
+++ theme/src/components/header.tsx
@@ -1,30 +1,26 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
-import { graphql, Link, StaticQuery } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
 import React from "react";
-import { Box } from "theme-ui";
+import { jsx, Box } from "theme-ui";
 
 export default function Header() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query HeaderQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          title
         }
-      `}
-      render={(data) => (
-        <header>
-          <Box py={2} px={3} sx={{ borderBottom: "1px solid", borderColor: "gray" }}>
-            <Link to="/" sx={{ fontWeight: "bold", color: "text", textDecoration: "none" }}>
-              {data.site.siteMetadata.title}
-            </Link>
-          </Box>
-        </header>
-      )}
-    />
+      }
+    }
+  `);
+
+  return (
+    <header>
+      <Box py={2} px={3} sx={{ borderBottom: "1px solid", borderColor: "gray" }}>
+        <Link to="/" sx={{ fontWeight: "bold", color: "text", textDecoration: "none" }}>
+          {data.site.siteMetadata.title}
+        </Link>
+      </Box>
+    </header>
   );
 }
