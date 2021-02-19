48126f44bbf5b897ac979b02e74798b27d8ae678
Eran Kampf <eran@ekampf.com>
2021-02-18 23:35:56 -0800

    Unecessary styles

--- theme/src/components/tippy.tsx
+++ theme/src/components/tippy.tsx
@@ -1,8 +1,6 @@
 /** @jsx jsx */
 import Tippy, { TippyProps } from "@tippyjs/react";
 import { jsx } from "theme-ui";
-import "tippy.js/dist/tippy.css";
-import "tippy.js/themes/light-border.css";
 import "tippy.js/animations/shift-away-extreme.css";
 
 export default function StyledTippy(props: TippyProps) {
