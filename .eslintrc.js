0df576cdbcb5bfd633d5c5898a6cdb97dc099b22
Eran Kampf <eran@ekampf.com>
2021-03-05 17:16:50 -0800

    Upgrade Gatsby to V3
    commit 08bca1e1eb3d16c4453de64e6917187a4f114bd8
    Author: Eran Kampf <eran@ekampf.com>
    Date:   Fri Mar 5 17:09:45 2021 -0800
    
        Upgraded all packages and Gatsby to v3
    
    commit b68886edf59045182cd9ee598fbd1767d6e2e472
    Author: Eran Kampf <eran@ekampf.com>
    Date:   Fri Mar 5 16:39:37 2021 -0800
    
        Upgraded to nodejs 14.16.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 const OFF = 0;
-const WARNING = 1;
+// const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
@@ -29,7 +29,7 @@ module.exports = {
     // React
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     // Prettier
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "prettier", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     // Import
     "plugin:import/recommended",
