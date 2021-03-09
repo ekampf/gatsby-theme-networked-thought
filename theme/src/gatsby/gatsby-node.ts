a7ff5ede5efb4c15e7fb928665c7f78eb96ddd11
Eran Kampf <eran@ekampf.com>
2021-03-08 19:21:35 -0800

    Fix types

--- theme/src/gatsby/gatsby-node.ts
+++ theme/src/gatsby/gatsby-node.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
+import { NodePluginArgs } from "gatsby";
 import createPages from "./create-pages";
 import createSchemaCustomization from "./create-schema-customization";
 import pluginOptionsSchema from "./plugin-options-schema";
+import type { PluginOptions } from "./plugin-options-schema";
 import sourceNodes from "./source-nodes";
 
-function onPreBootstrap({ reporter }, themeOptions) {
+function onPreBootstrap({ reporter }: NodePluginArgs, themeOptions: PluginOptions) {
   const thoughtsDirectory = themeOptions.thoughtsDirectory || "content/thoughts/";
   if (!fs.existsSync(thoughtsDirectory)) {
     reporter.info(`Creating notes directory: ${thoughtsDirectory}`);
